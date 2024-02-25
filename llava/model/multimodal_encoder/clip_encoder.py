34cfe91f9c9fbd144491ccdb2ba49d77b2fc4c12
Peter Tong <tsb@berkeley.edu>
2024-02-25 16:21:54 -0500

    Update clip_encoder.py

--- llava/model/multimodal_encoder/clip_encoder.py
+++ llava/model/multimodal_encoder/clip_encoder.py
@@ -44,8 +44,8 @@ class CLIPVisionTower(nn.Module):
         # Very Important for TorchXLA
         #self.vision_tower.vision_model.encoder.gradient_checkpointing = False
         
-        # from torch_xla.utils.checkpoint import checkpoint
-        # self.vision_tower.vision_model.encoder._gradient_checkpointing_func = checkpoint 
+        from torch_xla.utils.checkpoint import checkpoint
+        self.vision_tower.vision_model.encoder._gradient_checkpointing_func = checkpoint 
         
 
         if type(images) is list:
