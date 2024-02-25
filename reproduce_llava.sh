434b1c18f1e3bdcd3f5e71acb84e36b9adab82d4
Peter Tong <tsb@berkeley.edu>
2024-02-25 01:09:21 -0500

    Update reproduce_llava.sh

--- reproduce_llava.sh
+++ reproduce_llava.sh
@@ -18,7 +18,7 @@ python llava/train/train_tpu.py \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 10 \
+    --save_steps 100 \
     --save_total_limit 1 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
