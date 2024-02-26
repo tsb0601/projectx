b879a1830e4e184ed67ee6f936f3f1e877458af0
tsb0601 <64496620+tsb0601@users.noreply.github.com>
2024-02-26 13:14:02 -0500

    Update pretrain_spmd.sh

--- pretrain_spmd.sh
+++ pretrain_spmd.sh
@@ -4,23 +4,23 @@ python llava/train/train_spmd.py \
     --model_name_or_path /mnt/disks/storage/llm_ckpts/vicuna1.5 \
     --version v1 \
     --data_path merged_llava.json \
-    --image_folder /mnt/disks/storage/data/pretrain_data \
     --image_folder /mnt/disks/storage/data \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir ./checkpoints/llava-v1.5-13b-pretrain \
+    --output_dir ./checkpoints/llava-v1.5-13b \
     --num_train_epochs 1 \
     --per_device_train_batch_size 256 \
+    --group_by_modality_length True \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 24000 \
     --save_total_limit 1 \
-    --learning_rate 1e-3 \
+    --learning_rate 2e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
@@ -28,6 +28,6 @@ python llava/train/train_spmd.py \
     --tf32 False \
     --model_max_length 2048 \
     --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
+    --dataloader_num_workers 8 \
     --lazy_preprocess True \
     --report_to wandb
