8278a7aecafb7f00470f0aefb6f995d476dda166
Peter Tong <tsb@berkeley.edu>
2024-02-27 01:46:26 -0500

    Update reproduce_llava.sh

--- reproduce_llava.sh
+++ reproduce_llava.sh
@@ -12,7 +12,7 @@ python llava/train/train_tpu.py \
     --bf16 True \
     --output_dir ./checkpoints/llava-v1.5-13b \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 22 \
+    --per_device_train_batch_size 20 \
     --group_by_modality_length True \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
