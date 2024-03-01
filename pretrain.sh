62d526609e4e7cded996c41b780eaf4a8117abf1
Peter Tong <tsb@berkeley.edu>
2024-03-01 02:35:01 -0500

    Update pretrain.sh

--- pretrain.sh
+++ pretrain.sh
@@ -14,7 +14,7 @@ python llava/train/train_tpu.py \
     --bf16 True \
     --output_dir ./checkpoints/llava-v1.5-7b-stageI \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 2 \
+    --per_device_train_batch_size 4\
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
