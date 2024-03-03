328d983c4fe58c536048cae58a80a864a7416052
Peter Tong <tsb@berkeley.edu>
2024-03-02 23:06:25 -0500

    Create finetune_fsdp_merge.sh

---
+++ finetune_fsdp_merge.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+python llava/train/train_tpu.py \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
+    --version v1 \
+    --data_path merged_llava.json \
+    --image_folder /mnt/disks/storage/data \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 False \
+    --output_dir ./checkpoints/llava-v1.5-7b-finetune \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 10000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 False \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --fsdp "full_shard" \
+    --fsdp_config fsdp_config.json
