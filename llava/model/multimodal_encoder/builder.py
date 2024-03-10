9c73507e8dff5fbfa99db159b1b78eb124d29a1c
Peter Tong <tsb@berkeley.edu>
2024-03-10 17:51:25 -0400

    Update builder.py

--- llava/model/multimodal_encoder/builder.py
+++ llava/model/multimodal_encoder/builder.py
@@ -7,5 +7,4 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
     is_absolute_path_exists = os.path.exists(vision_tower)
     #if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion") or "ShareGPT4V" in vision_tower:
     return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
-
     #raise ValueError(f'Unknown vision tower: {vision_tower}')
