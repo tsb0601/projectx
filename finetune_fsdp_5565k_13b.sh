e7bd5c7c26c717e771f63b78b2429438da952ccb
Peter Tong <tsb@berkeley.edu>
2024-03-11 22:00:37 -0400

    Update finetune_fsdp_5565k_13b.sh

--- finetune_fsdp_5565k_13b.sh
+++ finetune_fsdp_5565k_13b.sh
@@ -13,7 +13,7 @@ python llava/train/train_tpu.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 False \
-    --output_dir ./checkpoints/llava-v1.5-7b-finetune-5565k \
+    --output_dir ./checkpoints/llava-v1.5-13b-finetune-5565k \
     --num_train_epochs 1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
