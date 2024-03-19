8647a4cda3331b4f503a703adafe5410893a1993
Peter Tong <tsb@berkeley.edu>
2024-03-18 23:31:46 -0400

    add

--- finetune_fsdp_5565k_eva.sh
+++ finetune_fsdp_5565k_eva.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 python llava/train/train_tpu.py \
-    --model_name_or_path lmsys/vicuna-33b-v1.3 \
+    --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
     --data_path /mnt/disks/storage/data/finetune_data/5565kL.jsonl \
     --image_folder /mnt/disks/storage/data/finetune_data \
-    --vision_tower openai/clip-vit-large-patch14-336 \
+    --vision_tower apple/DFN5B-CLIP-ViT-H-14-378 \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
@@ -13,10 +13,9 @@ python llava/train/train_tpu.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 False \
-    --output_dir ./checkpoints/llava-v1.5-33b-finetune-5565k \
+    --output_dir ./checkpoints/llava-v1.5-7b-finetune-5565k-dfnclip \
     --num_train_epochs 1 \
     --per_device_train_batch_size 4 \
-    --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
@@ -36,4 +35,3 @@ python llava/train/train_tpu.py \
     --report_to wandb \
     --fsdp "full_shard" \
     --fsdp_config fsdp_config.json
-
