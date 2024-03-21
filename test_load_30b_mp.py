1a449b8de54a11c270b5dc0cfcc8abe0523fbb91
Ellis Brown <ellis.l.brown.ii@gmail.com>
2024-03-21 13:53:28 -0400

    add mp test load

---
+++ test_load_30b_mp.py
@@ -0,0 +1,35 @@
+import transformers
+from ezcolorlog import log_stdout, root_logger as logger
+
+
+@log_stdout
+def load(model_name):
+    print("transformers.LlamaForCausalLM")
+    print(f"Loading LLM: {model_name}")
+    model = transformers.LlamaForCausalLM.from_pretrained(
+        model_name,
+        attn_implementation=None,
+        torch_dtype=None,
+    )
+    print(f"Loaded model {model_name}")
+
+
+
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default="lmsys/vicuna-7b-v1.5")
+    args = parser.parse_args()
+
+    # load(args.model_name)
+
+    import multiprocessing as mp
+    import torch_xla.distributed.xla_multiprocessing as xmp
+
+    logger.info("Starting XMP spawn")
+
+    mp.set_start_method('spawn', force=True)
+    xmp.spawn(load, args=(args.model_name,))
+    logger.info("Finished XMP spawn")
