04bba15cc3befd3c3e4a911e8aca1637663e8929
Peter Tong <tsb@berkeley.edu>
2024-03-24 02:36:33 -0400

    Update train_fsdp.py

--- llava/train/train_fsdp.py
+++ llava/train/train_fsdp.py
@@ -1220,8 +1220,8 @@ def train(INDEX, attn_implementation=None):
 				)
 			else:
 				config = transformers.AutoConfig.from_pretrained(model_args.model_name_or_path)
-				with torch.device("meta"):
-					model = LlavaLlamaForCausalLM(config, do_sample=True)
+				#with torch.device("meta"):
+				model = LlavaLlamaForCausalLM(config, do_sample=True)
 	else:
 		model = transformers.LlamaForCausalLM.from_pretrained(
 			model_args.model_name_or_path,
