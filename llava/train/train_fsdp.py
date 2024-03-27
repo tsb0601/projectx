be3379fd4f75921cb46252194aef7913730fabbd
Ellis Brown <ellis.l.brown.ii@gmail.com>
2024-03-27 15:19:45 -0400

    fix log

--- llava/train/train_fsdp.py
+++ llava/train/train_fsdp.py
@@ -1217,13 +1217,13 @@ def train(INDEX, attn_implementation=None):
         elif 'mistral' in model_args.model_name_or_path.lower():
             logger.warning(f"Vision tower, loading LlavaMistralForCausalLM: {model_args.model_name_or_path}")
 
-            # replace training_args.fsdp_config.fsdp_transformer_layer_cls_to_wrap with MistralDecoderLayer
+            # replace training_args.fsdp_config.transformer_layer_cls_to_wrap with MistralDecoderLayer
             logger.error(f"training_args.fsdp_config: {training_args.fsdp_config}")
             if (
                 hasattr(training_args, 'fsdp_config') and
                 'transformer_layer_cls_to_wrap' in training_args.fsdp_config.keys()
             ):
-                logger.warning(f"Replacing training_args.fsdp_config.fsdp_transformer_layer_cls_to_wrap with MistralDecoderLayer. Previous value: {training_args.fsdp_config.transformer_layer_cls_to_wrap}")
+                logger.warning(f"Replacing training_args.fsdp_config.transformer_layer_cls_to_wrap with MistralDecoderLayer. Previous value: {training_args.fsdp_config['transformer_layer_cls_to_wrap']}")
                 training_args.fsdp_config["transformer_layer_cls_to_wrap"] = ["MistralDecoderLayer"]
 
             model = LlavaMistralForCausalLM.from_pretrained(
