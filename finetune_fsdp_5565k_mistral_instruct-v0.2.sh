a7482efc64e93de7654bf3b3df45c00f29388234
Ellis Brown <ellis.l.brown.ii@gmail.com>
2024-03-31 20:01:18 -0400

    mistral-instruct-v0.2

---
+++ finetune_fsdp_5565k_mistral_instruct-v0.2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+export PJRT_DEVICE=TPU &&
+export XLA_USE_BF16=0 &&
+export WANDB_ENTITY=nyu-visionx && export WANDB_PROJECT=llava &&
+export CKPT_NAME="mistral-instruct-v0.2-7b-finetune-5565k-bs512" &&
+python llava/train/train_tpu.py \
+    --model_name_or_path mistralai/Mistral-7B-Instruct-v0.2 \
+    --version v1 \
+    --data_path /mnt/disks/storage/data/finetune_data/5565kL.jsonl \
+    --image_folder /mnt/disks/storage/data/finetune_data \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 False \
+    --output_dir ./checkpoints/$CKPT_NAME \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 100000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 False \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --run_name $CKPT_NAME \
+    --fsdp "full_shard" \
+    --fsdp_config fsdp_config.json \
+    # --gcp_project "nyu-vision-lab" \
+    # --gcs_output_dir "gs://us-central2-storage/cambrian/checkpoints/$CKPT_NAME"
+
