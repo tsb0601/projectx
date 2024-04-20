d438a7a29c32f9e60444aa21567963869e3aa758
Peter Tong <tsb@berkeley.edu>
2024-04-20 01:16:49 -0400

    Update llava_arch.py

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -198,14 +198,16 @@ class LlavaMetaModel:
 
 		self.config.mm_vision_tower = vision_tower
 
+		
 		if self.get_vision_tower() is None:
 			vision_tower = build_vision_tower(model_args)
-
-			if (fsdp is not None and len(fsdp) > 0) or \
-                    (hasattr(model_args, 'unfreeze_mm_vision_tower') and model_args.unfreeze_mm_vision_tower):
-				self.vision_tower = [vision_tower]
-			else:
+			if model_args.unfreeze_mm_vision_tower:
 				self.vision_tower = vision_tower
+			else:
+				if (fsdp is not None and len(fsdp) > 0):
+					self.vision_tower = [vision_tower]
+				else:
+					self.vision_tower = vision_tower
 		else:
 			if fsdp is not None and len(fsdp) > 0:
 				vision_tower = self.vision_tower[0]
