bae7c795f3fd8172facf2a83584ef4ba0ff74357
Peter Tong <tsb@berkeley.edu>
2024-04-20 15:04:19 -0400

    Update pretrain.sh

--- pretrain.sh
+++ pretrain.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+export PJRT_DEVICE=TPU
+export XLA_USE_BF16=0
+export WANDB_ENTITY=nyu-visionx
+export WANDB_PROJECT=llava
+export CKPT_NAME="TPU-llava-v1.5-7b-pretrain"
+
 
 python llava/train/train_tpu.py \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version plain \
-    --data_path /mnt/disks/storage/data/pretrain_data/blip_laion_cc_sbu_558k.json \
+    --data_path /mnt/disks/storage/data/finetune_data/pretrain.jsonl \
     --image_folder /mnt/disks/storage/data/pretrain_data \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
@@ -12,16 +18,16 @@ python llava/train/train_tpu.py \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 False \
-    --output_dir ./checkpoints/llava-v1.5-7b \
+    --output_dir ./checkpoints/llava-v1.5-7b-pretrain \
     --num_train_epochs 1 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 24000 \
+    --save_steps 100000 \
     --save_total_limit 1 \
-    --learning_rate 1e-3 \
+    --learning_rate 2e-3 \
     --weight_decay 0. \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
@@ -31,4 +37,5 @@ python llava/train/train_tpu.py \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
-    --report_to wandb
+    --report_to wandb \
+    --run_name $CKPT_NAME \
