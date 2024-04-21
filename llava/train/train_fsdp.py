a93d11f6dc3f4204954a2358bb2497709cfc0ecf
Peter Tong <tsb@berkeley.edu>
2024-04-21 00:50:25 -0400

    Update train_fsdp.py

--- llava/train/train_fsdp.py
+++ llava/train/train_fsdp.py
@@ -203,6 +203,31 @@ def safe_save_model_for_hf_trainer(trainer: transformers.Trainer,
                                    output_dir: str):
     """Collects the state dict and dump to disk."""
 
+    # if getattr(trainer.args, "tune_mm_mlp_adapter", False):
+    #     # Only save Adapter
+    #     keys_to_match = ['mm_projector']
+    #     if getattr(trainer.args, "use_im_start_end", False):
+    #         keys_to_match.extend(['embed_tokens', 'embed_in'])
+
+    #     weight_to_save = get_mm_adapter_state_maybe_zero_3(trainer.model.named_parameters(), keys_to_match)
+    #     if trainer.args.local_rank == 0 or trainer.args.local_rank == -1:
+    #         trainer.model.config.save_pretrained(output_dir)
+    #     import torch_xla.core.xla_model as xm
+    #     ckpt_prefix = os.path.join(output_dir, "mm_projector")
+        
+    #     os.makedirs(output_dir, exist_ok=True)
+    #     rank = xm.get_ordinal()
+    #     print(rank)
+    #     world_size = xm.xrt_world_size()
+    #     ckpt_path = f'{ckpt_prefix}_rank-{rank:08d}-of-{world_size:08d}.pth'
+    #     ckpt = {
+    #         'model': weight_to_save,
+    #         'shard_metadata': trainer.model.get_shard_metadata()
+    #     }
+    #     os.makedirs(os.path.dirname(ckpt_path), exist_ok=True)
+    #     xm.save(ckpt, ckpt_path, master_only=False)
+    #     print(f'checkpoint saved to {ckpt_path}\n', end='')
+    #     return
     if getattr(trainer.args, "tune_mm_mlp_adapter", False):
         # Only save Adapter
         keys_to_match = ['mm_projector']
@@ -210,23 +235,17 @@ def safe_save_model_for_hf_trainer(trainer: transformers.Trainer,
             keys_to_match.extend(['embed_tokens', 'embed_in'])
 
         weight_to_save = get_mm_adapter_state_maybe_zero_3(trainer.model.named_parameters(), keys_to_match)
+        trainer.model.config.save_pretrained(output_dir)
+
+        current_folder = output_dir.split('/')[-1]
+        parent_folder = os.path.dirname(output_dir)
         if trainer.args.local_rank == 0 or trainer.args.local_rank == -1:
-            trainer.model.config.save_pretrained(output_dir)
-        import torch_xla.core.xla_model as xm
-        ckpt_prefix = os.path.join(output_dir, "mm_projector")
-        
-        os.makedirs(output_dir, exist_ok=True)
-        rank = xm.get_ordinal()
-        print(rank)
-        world_size = xm.xrt_world_size()
-        ckpt_path = f'{ckpt_prefix}_rank-{rank:08d}-of-{world_size:08d}.pth'
-        ckpt = {
-            'model': weight_to_save,
-            'shard_metadata': trainer.model.get_shard_metadata()
-        }
-        os.makedirs(os.path.dirname(ckpt_path), exist_ok=True)
-        xm.save(ckpt, ckpt_path, master_only=False)
-        print(f'checkpoint saved to {ckpt_path}\n', end='')
+            if current_folder.startswith('checkpoint-'):
+                mm_projector_folder = os.path.join(parent_folder, "mm_projector")
+                os.makedirs(mm_projector_folder, exist_ok=True)
+                torch.save(weight_to_save, os.path.join(mm_projector_folder, f'{current_folder}.bin'))
+            else:
+                torch.save(weight_to_save, os.path.join(output_dir, f'mm_projector.bin'))
         return
 
     if trainer.deepspeed:
