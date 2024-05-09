ad4fdfc3c23270cc90c4c4a105ba3d3c243b9396
tsb0601 <64496620+tsb0601@users.noreply.github.com>
2024-05-09 23:26:08 +0200

    pad

--- pretrain_siglipdinoclip.sh
+++ pretrain_siglipdinoclip.sh
@@ -15,6 +15,7 @@ python llava/train/train_tpu.py \
     --mm_projector_type mlp2x_gelu \
     --tune_mm_mlp_adapter True \
     --mm_vision_select_layer -2 \
+    --image_aspect_ratio pad \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 False \
