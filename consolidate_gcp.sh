d4fd769c8492b07a3535c5826c46d44a3dcd6c8f
tsb0601 <64496620+tsb0601@users.noreply.github.com>
2024-05-10 12:47:52 +0200

    Update consolidate_gcp.sh

--- consolidate_gcp.sh
+++ consolidate_gcp.sh
@@ -1,9 +1,6 @@
 # 1 arg = gcp link
 gcp_link=$1
 
-filename="mm_projector"
+cd ~/projectx
 
-# 1. download the file
-ckpt_path=./checkpoints/$filename
-echo "Downloading from $gcp_link to $ckpt_path"
-gcloud alpha storage rsync $gcp_link $ckpt_path
+gsutil cp $gcp_link .
