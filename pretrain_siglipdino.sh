424b29ce0b502cd4e3dd3ae42dffb3695ade8816
tsb0601 <64496620+tsb0601@users.noreply.github.com>
2024-05-12 09:58:28 +0200

    Update pretrain_siglipdino.sh

--- pretrain_siglipdino.sh
+++ pretrain_siglipdino.sh
@@ -3,7 +3,7 @@ export PJRT_DEVICE=TPU
 export XLA_USE_BF16=0
 export WANDB_ENTITY=nyu-visionx
 export WANDB_PROJECT=llava
-export CKPT_NAME="pretrain-siglip-dino-unpadII"
+export CKPT_NAME="pretrain-siglip-dino-unpadIII"
 
 
 python llava/train/train_tpu.py \
