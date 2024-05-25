fca7afd9828caa0baf3703a5b18613e58be59558
Peter Tong <tsb@berkeley.edu>
2024-05-25 04:46:11 -0400

    Update finetune_3637k.sh

--- finetune_3637k.sh
+++ finetune_3637k.sh
@@ -9,7 +9,7 @@ export CKPT_NAME="data-3637k"
 python llava/train/train_tpu.py \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
-    --data_path /mnt/disks/storage/data/finetune_data/jsons/3637kL.jsonl \
+    --data_path /mnt/disks/storage/data/finetune_data/3637kL.jsonl \
     --image_folder /mnt/disks/storage/data/finetune_data \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
